27c9e3227e5ed0d674b2fa2bc547e1ff8219888d
manjeete <manjeetkr2017@gmail.com>
2022-06-16 16:36:02 +0530

    testing deployment

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,7 +13,7 @@ const App = () => {
     <div style={{background: '#FFE4CC' , position: 'absolute', width: '1920px', height: '1080px',left: '0px',top: '0px'}}>
     <div style={{ marginLeft:'600px' , marginTop:'200px' , backgroundColor:'#FFFFFF' , border: '1px solid' , width: '400px',height: '300px',
     borderRadius: '16px' , boxShadow:'0px 4px 12px rgba(90, 41, 5, 0.12)' , paddingLeft : '45px' , paddingTop:'10px'}}>
-    <h1  style={{fontFamily: 'Nunito',fontStyle: 'normal',fontWeight: '800',fontSize: '35px',lineHeight: '140%'}}>Sign In</h1>
+    <h1  style={{fontFamily: 'Nunito',fontStyle: 'normal',fontWeight: '800',fontSize: '35px',lineHeight: '140%'}}>Sign In Out</h1>
       <Form
       name="normal_login"
       className="login-form"
